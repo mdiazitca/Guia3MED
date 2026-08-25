2718c2b448c6edc50b2b368ed33ad360a8feedba
mdiazitca <maria.diaz25@itca.edu.sv>
2026-08-24 22:01:50 -0600

    agregando ejerccio 2

---
+++ ejercicio2guia3MED/src/ejercicio2guia3med/Ejercicio2guia3MED.java
@@ -0,0 +1,24 @@
+
+package ejercicio2guia3med;
+import java.util.Arrays;
+public class Ejercicio2guia3MED {
+
+    public static void main(String[] args) {
+        int[] datos = {1, 5, 8, 9, 2, 3, 1};
+
+        System.out.println("Array original: " + Arrays.toString(datos));
+
+     
+        Arrays.sort(datos);
+        System.out.println("De menor a mayor: " + Arrays.toString(datos));
+
+        
+        System.out.print("De mayor a menor: [");
+        for (int i = datos.length - 1; i >= 0; i--) {
+            System.out.print(datos[i] + (i > 0 ? ", " : ""));
+        }
+        System.out.println("]");
+        // TODO code application logic here
+    }
+    
+}
